4dd42c49d0e13972d89e9e8ecb38092c02b41bb4
Rio Edwards <rioredwards@gmail.com>
2023-01-30 12:32:16 -0800

    Setup bootstrap and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header.js';
+import Main from './components/Main/Main.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">
+          <img
+            src="assets/todo-logo.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="ToDo's Logo"
+          />
+          {'  '}
+          Todos
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Sign Out</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
