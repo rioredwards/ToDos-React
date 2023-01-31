b5ec6fd1af04eb897d183854ab83f8fe43125494
Rio Edwards <rioredwards@gmail.com>
2023-01-30 19:41:22 -0800

    Add new todo's

--- src/components/Items/AddItemForm/AddItemForm.js
+++ src/components/Items/AddItemForm/AddItemForm.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { useState } from 'react';
+import { useItems } from '../../../context/ItemsContext.js';
+import { createListItem } from '../../../services/items.js';
 
 export default function AddItemForm() {
+  const [description, setDescription] = useState('');
+  const { setItems } = useItems();
+
+  const handleAddTodo = async () => {
+    try {
+      const item = await createListItem(description);
+      setItems((prev) => [item, ...prev]);
+      setDescription('');
+    } catch (e) {
+      console.error(e);
+    }
+    console.log(description);
+  };
+
   return (
-    <form>
-      <input name="description" required placeholder="add a todo"></input>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleAddTodo(description);
+      }}
+    >
+      <input
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        name="description"
+        required
+        placeholder="add a todo"
+      />
       <button>Add</button>
     </form>
   );
--- src/services/items.js
+++ src/services/items.js
@@ -1,11 +1,11 @@
 import { checkError, client } from './client.js';
 
 export async function getListItems() {
-  const resp = await client.from('todos').select();
+  const resp = await client.from('todos').select().order('id', { ascending: false });
 
   return checkError(resp);
 }
-export async function createListItems({ description }) {
+export async function createListItem(description) {
   const resp = await client.from('todos').insert([{ description }]).single();
 
   return checkError(resp);
